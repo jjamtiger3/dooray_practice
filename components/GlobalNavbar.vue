64698fa04e4b97279b011b79a748bc7d31fadae3
seois830926 <idmahoro@gmail.com>
2021-05-09 10:13:13 +0900

    font-awesome 추가 메뉴바 컴포넌트 추가

---
+++ components/GlobalNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="global-navbar">
+        <button class="fa fa-clock-o"></button>
+        <a href="">프로젝트</a>
+        <a href="">메일</a>
+        <a href="">캘린더</a>
+        <a href="">드라이브</a>
+        <a href="">위키</a>
+        <a href="">주소록</a>
+    </div>
+</template>
+<style scoped>
+    @import url('../node_modules/font-awesome/css/font-awesome.min.css');
+    @import url('../assets/css/global-navbar/global-navbar.css');
+</style>
